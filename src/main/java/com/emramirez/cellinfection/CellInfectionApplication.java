7f21e84cb9291c26ad3a0cb0d2ca3f1988ce0fcd
ramsysproject <m.ramirez05@gmail.com>
2019-09-04 16:56:21 -0300

    Fix indentation

--- src/main/java/com/emramirez/cellinfection/CellInfectionApplication.java
+++ src/main/java/com/emramirez/cellinfection/CellInfectionApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Slf4j
 public class CellInfectionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CellInfectionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CellInfectionApplication.class, args);
+    }
 
 }
--- src/test/java/com/emramirez/cellinfection/service/IterativeInfectionProcessorTest.java
+++ src/test/java/com/emramirez/cellinfection/service/IterativeInfectionProcessorTest.java
@@ -17,38 +17,38 @@ import static org.junit.Assert.assertEquals;
 @Slf4j
 public class IterativeInfectionProcessorTest {
 
-	public static final int ROWS = 6;
-	public static final int COLUMNS = 10;
+    public static final int ROWS = 6;
+    public static final int COLUMNS = 10;
     public static final int EXAMPLE_INFECTED_CELLS_QUANTITY = 12;
 
     @Autowired
     IterativeInfectionProcessor service;
 
-	@Test
-	public void getInfection_exampleMatrixGiven_12infectedCellsExpected() {
-		// arrange
+    @Test
+    public void getInfection_exampleMatrixGiven_12infectedCellsExpected() {
+        // arrange
         int[][] resistances = buildResistancesMatrix();
         Cell[][] cellMatrix = buildCellsMatrix(resistances);
         service.initialize(cellMatrix);
 
-		// act
+        // act
         Set<Cell> result = service.getInfection(cellMatrix[2][3]);
 
-		// assert
+        // assert
         result.stream().forEach(cell -> log.info("Infected cell found in row {} and column {} with resistance {}",
                 cell.getRowPosition() + 1, cell.getColumnPosition() + 1, cell.getResistance()));
         assertEquals("Wrong infected cells number!", EXAMPLE_INFECTED_CELLS_QUANTITY, result.size());
-	}
+    }
 
     private int[][] buildResistancesMatrix() {
-        return new int[][] {
-                    {8, 7, 4, 1, 2, 8, 3, 6, 9, 8},
-                    {9, 3, 4, 2, 7, 6, 8, 3, 4, 4},
-                    {6, 7, 2, 9, 6, 4, 9, 0, 4, 6},
-                    {7, 9, 7, 5, 2, 9, 8, 7, 7, 0},
-                    {2, 6, 2, 4, 3, 5, 4, 0, 9, 7},
-                    {1, 2, 5, 0, 6, 2, 8, 5, 7, 3},
-            };
+        return new int[][]{
+                {8, 7, 4, 1, 2, 8, 3, 6, 9, 8},
+                {9, 3, 4, 2, 7, 6, 8, 3, 4, 4},
+                {6, 7, 2, 9, 6, 4, 9, 0, 4, 6},
+                {7, 9, 7, 5, 2, 9, 8, 7, 7, 0},
+                {2, 6, 2, 4, 3, 5, 4, 0, 9, 7},
+                {1, 2, 5, 0, 6, 2, 8, 5, 7, 3},
+        };
     }
 
     private Cell[][] buildCellsMatrix(int[][] resistances) {
